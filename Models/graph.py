9700a6f9e3616f178f54ffce0b513c56413161f9
Hamed Babagheybi <hbabagheybi@gmail.com>
2020-06-30 16:20:05 +0430

    graph.py: extra sys import removed. get_num_of_vertices function defined. main function and is_valid_values function has been moved to run.py. code style improved.

--- Models/graph.py
+++ Models/graph.py
@@ -1,6 +1,3 @@
-import sys
-
-
 # This class represent a graph
 class Graph:
 
@@ -23,6 +20,12 @@ class Graph:
         return self.graph_dict.keys()
 
 
+    # Get number of vertices
+    def get_num_of_vertices(self):
+
+        return len(self.get_vertices())
+
+
     # Get neighbors or a neighbor
     def get_neighbors(self, parent):
 
@@ -41,17 +44,23 @@ class Graph:
         visited, queue = list(), list() # List to keep track of visited and queue nodes
 
         VMN = sorted(                                # The vertice with most neighbors
+            
             self.graph_dict.keys(),
+
             key=lambda x:len(self.graph_dict[x]),
+
             reverse = True
         )[0]
 
         visited.append(VMN)
+        
         queue.append(VMN)
 
         while queue:
 
-            start = queue.pop(0) 
+            start = queue.pop(0)
+
+            print(start)
 
             neighbours = self.get_neighbors(start)
             
@@ -60,74 +69,9 @@ class Graph:
                 if neighbour not in visited:
 
                     visited.append(neighbour)
+
                     queue.append(neighbour)
 
             if set(visited) == set(self.get_vertices()): return True
 
         return False
-
-
-# function to check if values are binary matrix or not 
-def is_valid_values(values):
-
-    for value in values:
-
-        if not (int(value) != 0 or int(value) != 1): return False
-
-    return True
-
-
-# Main function
-def main():
-
-
-    try:
-
-        num_of_vertics = int(input('The number of vertics: '))
-
-        try:
-
-            matrix = list()
-
-            for num in range(num_of_vertics):
-
-                input_tmp = list(input('{}. '.format(num + 1)).lower().split(" "))
-
-                while '' in input_tmp: input_tmp.remove('')
-                
-                if not is_valid_values(input_tmp): raise ValueError
-
-                if len(input_tmp) > num_of_vertics:
-                    
-                    sys.exit("The number of vertics can't be more than {}".format(num_of_vertics))
-
-                if input_tmp.count('1') >= num_of_vertics:
-
-                    sys.exit("The number of connected vertics can't be equal or more than {}".format(num_of_vertics))
-
-
-                matrix.append(tuple(input_tmp))
-
-        except (ValueError, NameError, SyntaxError):          # raise error message if matrix rows values are not binary and exit
-            
-            sys.exit("Matrix Values must be binary!")
-        
-    except (ValueError, NameError, SyntaxError):               # raise error message if num_of_vertics is not integer and exit
-
-        sys.exit('Not an integer! Please enter an integer.')
-
-    # Create a graph
-    graph = Graph()
-    
-    # Create graph connections
-    for row_index, row in enumerate(matrix):
-        
-        children = [index for index, child in enumerate(row) if child == '1']
-
-        graph.connect(row_index, tuple(children))
-
-    print("connected" if graph.is_connected() else "sorry")
-
-
-# Tell python to run main method
-if __name__ == "__main__": main()
